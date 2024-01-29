51bb94a797ca265cd618f1fcafe579b07ff8130e
André <abrizidobasilio@gmail.com>
2024-01-29 16:59:36 -0300

    realizado arquivo de insert do mysql

--- 014ConnectToDatabase/index.php
+++ 014ConnectToDatabase/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
     <title>LoginMySql</title>
 </head>
 <body>
@@ -15,6 +16,20 @@
         <input required name="pwd" type="password" placeholder="Password">
         <input required name="mail" type="email" placeholder="E-Mail">
         <button>Signup</button>
+    </form>
+
+    <h1>Delete</h1>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input required name="username" type="text" placeholder="Username">
+        <input required name="pwd" type="password" placeholder="Password">
+        <button>Delete</button>
+    </form>
+
+    <h1>Update</h1>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input required name="username" type="text" placeholder="Username">
+        <input required name="pwd" type="password" placeholder="Password">
+        <button>Update</button>
     </form>
 </body>
 </html>
